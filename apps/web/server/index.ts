b69be1107d6b69e352a93c67f35d4aeafe64413b
jin-Pro <dnjun2@ajou.ac.kr>
2024-09-18 04:00:56 +0900

    enhance: web server trpc UserRouter Split

--- apps/web/server/index.ts
+++ apps/web/server/index.ts
@@ -1,23 +1,8 @@
-import { z } from "zod";
-import db from "./db";
-import { publicProcedure, router } from "./trpc";
+import { router } from "./trpc";
+import UserRouter from "./user";
+
 const appRouter = router({
-  userList: publicProcedure.query(async () => {
-    const users = await db.user.findMany();
-    return users;
-  }),
-  userById: publicProcedure
-    .input(z.object({ id: z.string() }))
-    .query(async ({ input: { id } }) => {
-      const user = await db.user.findById({ id });
-      return user;
-    }),
-  userCreated: publicProcedure
-    .input(z.object({ name: z.string() }))
-    .mutation(async ({ input: { name } }) => {
-      const user = await db.user.create({ name });
-      return user;
-    }),
+  ...UserRouter,
 });
 
 export type AppRouter = typeof appRouter;
